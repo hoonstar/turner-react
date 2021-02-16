a1b452f676b1e25e8260bc0728429eb44be849e2
hongdotcom <hongdotcom@gmail.com>
2021-02-17 10:14:33 +1300

    remove useless

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React com on man!!!!!!!!!!! This is Azure AWS GITHUB PAGE Test
-          2!
-        </a>
-      </header>
+    <div>
+      <p>This is a start</p>
     </div>
   );
 }
